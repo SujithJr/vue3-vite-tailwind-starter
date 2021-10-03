6686738599cf7fd54470785b32e47e5f90b5131e
SujithJr <sujithsrt10@gmail.com>
2021-10-03 19:27:50 +0530

    setup complete

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="h-screen text-center mt-32">
+        <img src="./assets/logo.png" alt="Vue" class="inline-block" />
+        <h1 class="text-4xl text-gray-700 font-medium mt-8">Vue + Vite + TailwindCss</h1>
+    </div>
+</template>
+
+<script setup>
+    // This starter template is using Vue 3 <script setup> SFCs
+    // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+</script>
+
+<style>
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+    }
+</style>
